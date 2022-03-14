f3b1a546566533ed0acff7354667f93d9d6d5ab7
jaydip-9548 <jaydip@htree.plus>
2022-03-14 21:07:12 +0530

    TEst

---
+++ main.py
@@ -0,0 +1,20 @@
+from datetime import date
+
+class Person:
+    
+    def __init__(self, name,age):
+        self.name = name
+        self.age = age
+
+    @classmethod
+    def fromBirthYear(cls,name,year):
+        return cls(name,date.today().year - year)
+
+
+p1 = Person('jaydip',21)
+p2 = Person.fromBirthYear('kishan',2000)
+
+print(p1.age)
+print(p2.age)
+        
+    
